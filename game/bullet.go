6268c4b560971e274e719add05f2d21f2b5d006b
jame <cloudismydog@gmail.com>
2023-11-29 21:39:14 -0800

    implement Bullet struct and constructor

--- game/bullet.go
+++ game/bullet.go
@@ -1,1 +1,22 @@
 package game
+
+import (
+	_ "image/png"
+
+	"github.com/JolliestJames/ebiten-game/assets"
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+type Bullet struct {
+	sprite *ebiten.Image
+	position Vector
+	rotation float64
+}
+
+func NewBullet(p Vector, r float64) *Bullet {
+	return &Bullet{
+		position: p,
+		rotation: r,
+		sprite: assets.BulletSprite,
+	}
+}
