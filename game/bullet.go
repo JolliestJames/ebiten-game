b1968379209755c165cc62dba379e2f57d601475
jame <cloudismydog@gmail.com>
2023-11-30 10:07:57 -0800

    implement update and draw methods for bullet

--- game/bullet.go
+++ game/bullet.go
@@ -1,10 +1,15 @@
 package game
 
 import (
+	"math"
 	_ "image/png"
 
 	"github.com/JolliestJames/ebiten-game/assets"
 	"github.com/hajimehoshi/ebiten/v2"
+)
+
+const (
+	bulletSpeedPerSecond = 350.0
 )
 
 type Bullet struct {
@@ -19,4 +24,27 @@ func NewBullet(p Vector, r float64) *Bullet {
 		rotation: r,
 		sprite: assets.BulletSprite,
 	}
+}
+
+func (b *Bullet) Update() {
+	speed := bulletSpeedPerSecond / float64(ebiten.TPS())
+
+	b.position.X += math.Sin(b.rotation) * speed
+	b.position.Y += math.Cos(b.rotation) * -speed
+}
+
+func (b *Bullet) Draw(screen *ebiten.Image) {
+	bounds := b.sprite.Bounds()
+
+	halfW := float64(bounds.Dx()) / 2
+	halfH := float64(bounds.Dy()) / 2
+
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(-halfW, -halfH)
+	op.GeoM.Rotate(b.rotation)
+	op.GeoM.Translate(halfW, halfH)
+
+	op.GeoM.Translate(b.position.X, b.position.Y)
+
+	screen.DrawImage(b.sprite, op)
 }
