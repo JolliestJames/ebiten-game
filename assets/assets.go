afd80bc1fcf77a18e43d0c81768f91314986a3fe
jame <cloudismydog@gmail.com>
2023-11-30 22:35:24 -0800

    load font

--- assets/assets.go
+++ assets/assets.go
@@ -1,12 +1,14 @@
 package assets
 
 import (
-	"fmt"
 	"embed"
+	"fmt"
 	"image"
 	_ "image/png"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"golang.org/x/image/font"
+	"golang.org/x/image/font/opentype"
 )
 
 //go:embed *
@@ -15,6 +17,7 @@ var assets embed.FS
 var BulletSprite = mustLoadImage("missile.png")
 var PlayerSprite = mustLoadImage("player.png")
 var MeteorSprites = mustLoadImages("meteors")
+var ScoreFont = mustLoadFont("font.ttf")
 
 func mustLoadImage(name string) *ebiten.Image {
 	f, err := assets.Open(name)
@@ -45,3 +48,26 @@ func mustLoadImages(name string) []*ebiten.Image {
 
 	return images
 }
+
+func mustLoadFont(name string) font.Face {
+	f, err := assets.ReadFile(name)
+	if err != nil {
+		panic(err)
+	}
+
+	tt, err := opentype.Parse(f)
+	if err != nil {
+		panic(err)
+	}
+
+	face, err := opentype.NewFace(tt, &opentype.FaceOptions{
+		Size: 48,
+		DPI: 72,
+		Hinting: font.HintingVertical,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	return face
+} 
